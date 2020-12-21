f81958416dc16eac9008f0f768ba83028c3078a7
Fathima Reeza <fathimareeza9@yahoo.com>
2020-12-20 21:30:14 -0500

    Update ministat.c

--- ministat.c
+++ ministat.c
@@ -917,7 +917,7 @@ ReadPartition(void * part, int flag_INT, const char *n)
 
 
 static struct dataset *
-ReadSet(const char *n, int column, const char *delim, int t, flag_INT)
+ReadSet(const char *n, int column, const char *delim, int t, int flag_INT)
 {
 	int fileDescriptor;
 
